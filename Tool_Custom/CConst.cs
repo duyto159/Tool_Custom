b46da5242aea2edd02e09367344466ed137c65c7
duyto159 <duyto159@gmail.com>
2021-06-10 23:00:03 +0700

    duy

---
+++ Tool_Custom/CConst.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Custom
+{
+    public class CConst
+    {
+        public const int INT_VALUE_NULL = 0;
+        public const int FLT_VALUE_NULL = 0;
+        public const double DB_VALUE_NULL = 0;
+        public const string STR_VALUE_NULL = "";
+        public static DateTime DTM_VALUE_NULL = Convert_String_To_Datetime("01/01/1900", "dd/MM/yyyy");
+        public const int INT_VALUE_ALL = -5;
+        public const bool BL_VALUE_NULL = false;
+
+        private static DateTime Convert_String_To_Datetime(string p_strDate, string p_strFormat)
+        {
+            if (p_strDate == "")
+                return CConst.DTM_VALUE_NULL;
+
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            return DateTime.ParseExact(p_strDate, p_strFormat, provider);
+        }
+    }
+}
---
+++ Tool_Custom/Entities/CProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Custom.Entities
+{
+    public class CProduct
+    {
+        private string m_strName;
+        private string m_strLink;
+        private string m_strImage;
+        private double m_dblPrice;
+
+        public CProduct()
+        {
+            ResetData();
+        }
+        public void ResetData()
+        {
+            m_strName = CConst.STR_VALUE_NULL;
+            m_strLink = CConst.STR_VALUE_NULL;
+            m_strImage = CConst.STR_VALUE_NULL;
+            m_dblPrice = CConst.DB_VALUE_NULL;
+        }
+
+
+        public string Name { get => m_strName; set => m_strName = value; }
+        public string Link { get => m_strLink; set => m_strLink = value; }
+        public string Image { get => m_strImage; set => m_strImage = value; }
+        public double Price { get => m_dblPrice; set => m_dblPrice = value; }
+
+
+    }
+}
