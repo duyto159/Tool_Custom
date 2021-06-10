d66094cab536d29141afd29f6e557d3cb490ab34
duyto159 <duyto159@gmail.com>
2021-06-10 22:27:36 +0700

    b

--- Tool_Custom/frmTiki.Designer.cs
+++ Tool_Custom/frmTiki.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.rtbData = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(622, 55);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(109, 23);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(80, 55);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(511, 20);
+            this.txtLink.TabIndex = 1;
+            // 
+            // rtbData
+            // 
+            this.rtbData.Location = new System.Drawing.Point(12, 123);
+            this.rtbData.Name = "rtbData";
+            this.rtbData.Size = new System.Drawing.Size(765, 300);
+            this.rtbData.TabIndex = 2;
+            this.rtbData.Text = "";
+            // 
+            // frmTiki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbData);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.btnGet);
+            this.Name = "frmTiki";
             this.Text = "frmTiki";
+            this.Load += new System.EventHandler(this.frmTiki_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.RichTextBox rtbData;
     }
 }
--- Tool_Custom/frmTiki.cs
+++ Tool_Custom/frmTiki.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,55 @@ namespace Tool_Custom
 {
     public partial class frmTiki : Form
     {
+        HttpClient m_httpClient;
+
         public frmTiki()
         {
             InitializeComponent();
+            m_httpClient = new HttpClient();
+            txtLink.Text = "https://tiki.vn/thiet-bi-kts-phu-kien-so/c1815";
+
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string v_strHtml_Source = CrawlDataFromURL(txtLink.Text.Trim());
+            string v_strPattern = @"<a class=""product-item(.*)?</a>";
+            string v_strPattern2 = @"<a class=""product-item"" data-view-index=""\d+""(.*?)</a>";
+            var v_arrProduct = Regex.Matches(v_strHtml_Source, v_strPattern2);
+
+            StringBuilder v_sbResult = new StringBuilder();
+
+            foreach (var item in v_arrProduct)
+            {
+                string v_strName = Regex.Match(item.ToString(), @"<div class=""name(.*?)</span></div>").Value.Replace("<div>", "");
+                //v_sbResult.AppendLine(item.ToString() + "\n");
+                string v_strName_Key = Regex.Replace(v_strName, @"(<div (.*?)>)|(<span>)|(</span></div>)", "");
+                v_sbResult.AppendLine(v_strName_Key + "\n");
+            }
+
+
+            rtbData.Text = v_sbResult.ToString();
+
+
+        }
+
+        private string CrawlDataFromURL(string p_strUrl)
+        {
+            string v_strHtml = "";
+
+            v_strHtml = m_httpClient.GetStringAsync(p_strUrl).Result;
+
+            return v_strHtml;
+        }
+
+        private void frmTiki_Load(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    btnGet_Click(sender, e);
+            //    //Thread.Sleep(5000);
+            //}
         }
     }
 }
